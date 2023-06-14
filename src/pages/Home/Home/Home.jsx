630dd3f4ff5af5489a6440e9610d199e2b8fa771
Pijush Sarker <pijushsarker.dev@gmail.com>
2023-06-14 10:07:51 +0600

    top classes

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,10 +1,14 @@
 import HomeSlider from "../HomeSlider/HomeSlider";
+import TopClassSection from "../TopClassSection";
+import TopInstructor from "../TopInstructor";
 
 
 const Home = () => {
     return (
         <>
             <HomeSlider></HomeSlider>
+            <TopClassSection></TopClassSection>
+            <TopInstructor></TopInstructor>
         </>
     );
 };
---
+++ src/pages/Home/TopClassSection.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+
+const TopClassSection = () => {
+
+    const [topClass, setTopClass] = useState([]);
+
+
+    useEffect(() => {
+        fetch('http://localhost:5000/topClasses')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                setTopClass(data)
+            })
+    }, [])
+    return (
+        <div>
+            <h1 className="text-center font-bold text-4xl  my-10">Top 6 Classes</h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mb-10 gap-3">
+                {
+                    topClass.map(item => <div key={item._id}>
+
+                        <div className="card bg-base-100 shadow-xl">
+                            <figure><img className="h-56 w-full" src={item.classImage} alt="class image" /></figure>
+                            <div className="card-body">
+                                <h2 className="card-title">Class Name: {item.className}</h2>
+                                <p className="font-medium text-lg">Instructor: {item.instructorName}</p>
+                                <div className="card-actions justify-start">
+                                    <div className="badge hover:border-black badge-outline border-red-600">Seats: {item.seats}</div>
+                                    <div className="badge hover:border-black badge-outline border-red-600">Fees: {item.price}</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>)
+                }
+            </div>
+
+        </div>
+    );
+};
+
+export default TopClassSection;
