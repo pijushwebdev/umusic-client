72171c5db9c3161c452d1530bedd43bc8573218a
Pijush Sarker <pijushsarker.dev@gmail.com>
2023-08-11 01:01:42 +0600

    fixed issue

--- src/pages/Home/Newsletter.jsx
+++ src/pages/Home/Newsletter.jsx
@@ -10,8 +10,8 @@ const Newsletter = () => {
                     <p className='text-slate-500'>Subscribe for our latest & Articles. We Won’t Give You Spam Mails
                     </p>
                     <form>
-                        <label className="flex justify-center py-10 " htmlFor="email">
-                            <input type="email" className='py-2 px-4 text-white bg-transparent border w-1/4 rounded-3xl outline-none' name="email" placeholder='Enter email'/>
+                        <label className="flex justify-center py-10 ml-10 md:ml-0" htmlFor="email">
+                            <input type="email" className='py-2 px-4 text-white bg-transparent  border md:w-1/2 w-full lg:w-1/3 rounded-3xl outline-none' name="email" placeholder='Enter email'/>
                             <button className="relative -left-[52px] bg-[#DC2751] p-4 text-xl rounded-3xl"><FaRegPaperPlane /></button>
                         </label>
                     </form>
