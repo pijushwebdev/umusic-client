56129d49e06e456669365de9e6a316ef218954ef
pijushwebdev <pijushsarker.dev@gmail.com>
2025-01-01 11:43:04 +0600

    refractor slides

--- src/pages/Home/HomeSlider/HomeSlider.jsx
+++ src/pages/Home/HomeSlider/HomeSlider.jsx
@@ -42,148 +42,72 @@ const HomeSlider = () => {
 
   const slidesData = [
     {
-        title: 'Piano',
-        link: 'https://i.ibb.co/z4Jzkd2/female-student-playing-piano.jpg',
-        description: `Piano comes from the original Italian name for the instrument:
+      title: "Piano",
+      link: "https://i.ibb.co/z4Jzkd2/female-student-playing-piano.jpg",
+      description: `Piano comes from the original Italian name for the instrument:
                 piano e forte, soft and loud.Piano is also the musical notation
                 that tells the player that something should be played quietly`,
-        
     },
     {
-        title: 'Guitar',
-        link: 'https://i.ibb.co/YWndFwJ/man-with-guitar-darkness.jpg',
-        description: `You can definitely learn on an electric guitar, but overall,
+      title: "Guitar",
+      link: "https://i.ibb.co/YWndFwJ/man-with-guitar-darkness.jpg",
+      description: `You can definitely learn on an electric guitar, but overall,
                 acoustic guitar wins out every time. Its easier to sound good,
                 easier to play and its easier to learn`,
-        
     },
     {
-        title: 'Vocal',
-        link: 'https://i.ibb.co/qDnCYRy/modern-microphone.jpg',
-        description: `Vocal recording, in essence, is capturing the voice of the
+      title: "Vocal",
+      link: "https://i.ibb.co/qDnCYRy/modern-microphone.jpg",
+      description: `Vocal recording, in essence, is capturing the voice of the
                 artist in the most effective and efficient way so that the
                 play-back will be as true sounding to the original performance
                 as possible`,
-        
-    }
-  ]
+    },
+  ];
+  
   return (
     <div>
       <div ref={sliderRef} className="keen-slider mb-10">
-        <div className="keen-slider__slide number-slide1 relative ">
-          <div className=" overflow-hidden">
-            <img
-              className="bg-animation w-full"
-              src="https://i.ibb.co/qDnCYRy/modern-microphone.jpg"
-              alt=""
-            />
-          </div>
-
-          <div className="absolute left-3 md:left-10 md:top-10 lg:top-32 top-0 z-10">
-            <div className="content">
-              <h2 className="lg:text-8xl md:text-4xl text-2xl font-extrabold mt-5 md:mt-10 text-white">
-                Vocal <span className="block">Recording</span>
-              </h2>
+        {slidesData.map((slide, index) => (
+          <div
+            key={index}
+            className="keen-slider__slide number-slide1 relative "
+          >
+            <div className=" overflow-hidden">
+              <img
+                className="bg-animation w-full"
+                src={slide.link}
+                alt=""
+              />
             </div>
 
-            <Slide>
-              <p className="md:my-6 my-2 text-sm text-slate-300 font-normal md:font-medium w-full lg:w-1/2">
-                Vocal recording, in essence, is capturing the voice of the
-                artist in the most effective and efficient way so that the
-                play-back will be as true sounding to the original performance
-                as possible.{" "}
-              </p>
-            </Slide>
+            <div className="absolute left-3 md:left-10 md:top-10 lg:top-32 top-0 z-10">
+              <div className="content">
+                <h2 className="lg:text-8xl md:text-4xl text-2xl font-extrabold mt-5 md:mt-10 text-white">
+                  {slide.title} <span className="block">Recording</span>
+                </h2>
+              </div>
 
-            <button className="lg:py-4 lg:px-14 md:py-2 md:px-7 py-1 px-2 mt-3 md:mt-5 lg:mt-10 bg-[#DC2751] text-white font-semibold text-normal md:text-lg rounded-[30px]">
-              Explore More
-            </button>
-          </div>
+              <Slide>
+                <p className="md:my-6 my-2 text-sm text-slate-300 font-normal md:font-medium w-full lg:w-1/2">
+                  {slide.description}.{" "}
+                </p>
+              </Slide>
 
-          <div className="absolute left-1/2 hidden md:block border rounded-3xl py-5 border-white  bottom-40  translate-x-1/2 translate-y-1/2">
-            <span className="text-white text-3xl border-white">
-              <span className="animate-ping">
-                <BsArrowDownShort />
-              </span>
-            </span>
-          </div>
-        </div>
-
-        <div className="keen-slider__slide number-slide2 relative ">
-          <div className=" overflow-hidden">
-            <img
-              className="bg-animation w-full"
-              src="https://i.ibb.co/z4Jzkd2/female-student-playing-piano.jpg"
-              alt=""
-            />
-          </div>
-
-          <div className="absolute left-3 md:left-10 lg:top-32 md:top-10 top-0 z-10">
-            <div className="content">
-              <h2 className="lg:text-8xl md:text-4xl text-2xl font-extrabold mt-5 md:mt-10 text-white">
-                Piano <span className="block">Courses</span>
-              </h2>
+              <button className="lg:py-4 lg:px-14 md:py-2 md:px-7 py-1 px-2 mt-3 md:mt-5 lg:mt-10 bg-[#DC2751] text-white font-semibold text-normal md:text-lg rounded-[30px]">
+                Explore More
+              </button>
             </div>
 
-            <Slide>
-              <p className="md:my-6 my-2 text-sm text-slate-300 font-normal md:font-medium w-full lg:w-1/2">
-                Piano comes from the original Italian name for the instrument:
-                piano e forte, soft and loud.Piano is also the musical notation
-                that tells the player that something should be played quietly.{" "}
-              </p>
-            </Slide>
-
-            <button className="lg:py-4 lg:px-14 md:py-2 md:px-7 py-1 px-2 mt-3 md:mt-5 lg:mt-10 bg-[#DC2751] text-white font-semibold text-normal md:text-lg rounded-[30px]">
-              Explore More
-            </button>
-          </div>
-
-          <div className="fixed left-1/2 hidden md:block  border rounded-3xl py-5 border-white  bottom-40 translate-x-1/2 translate-y-1/2">
-            <span className="text-white text-3xl border-white">
-              <span className="animate-ping">
-                <BsArrowDownShort />
+            <div className="absolute left-1/2 hidden md:block border rounded-3xl py-5 border-white  bottom-40  translate-x-1/2 translate-y-1/2">
+              <span className="text-white text-3xl border-white">
+                <span className="animate-ping">
+                  <BsArrowDownShort />
+                </span>
               </span>
-            </span>
-          </div>
-        </div>
-
-        <div className="keen-slider__slide number-slide3 relative ">
-          <div className="overflow-hidden">
-            <img
-              className="bg-animation w-full"
-              src="https://i.ibb.co/YWndFwJ/man-with-guitar-darkness.jpg"
-              alt=""
-            />
-          </div>
-
-          <div className="absolute left-3 md:left-10 lg:top-32 md:top-10 top-0 z-10">
-            <div className="content">
-              <h2 className="lg:text-8xl md:text-4xl text-2xl font-extrabold mt-5 md:mt-10 text-white">
-                Guitar <span className="block">Courses</span>
-              </h2>
             </div>
-
-            <Slide>
-              <p className="md:my-6 my-2 text-sm text-slate-300 font-normal md:font-medium w-full lg:w-1/2">
-                You can definitely learn on an electric guitar, but overall,
-                acoustic guitar wins out every time. Its easier to sound good,
-                easier to play and its easier to learn{" "}
-              </p>
-            </Slide>
-
-            <button className="lg:py-4 lg:px-14 md:py-2 md:px-7 py-1 px-2 mt-3 md:mt-5 lg:mt-10 bg-[#DC2751] text-white font-semibold text-normal md:text-lg rounded-[30px]">
-              Explore More
-            </button>
           </div>
-
-          <div className="fixed left-1/2 hidden md:block  border rounded-3xl py-5 border-white  bottom-40 translate-x-1/2 translate-y-1/2">
-            <span className="text-white text-3xl border-white">
-              <span className="animate-ping">
-                <BsArrowDownShort />
-              </span>
-            </span>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
